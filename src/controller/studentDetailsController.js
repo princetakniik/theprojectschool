6c49f502ded57b7bff21257214158701a62974b7
Amit Kumar <sharma.amit@mounttalent.com>
2023-04-21 17:01:43 +0530

    update

--- src/controller/studentDetailsController.js
+++ src/controller/studentDetailsController.js
@@ -1,4 +1,4 @@
-const { studentdetails } = require("../Config/dbConnection");
+const { studentdetails, register } = require("../Config/dbConnection");
 const { QueryTypes } = require("sequelize");
 const db = require("../Config/dbConnection");
 
@@ -6,17 +6,26 @@ const createStudentsDetails = async (req, res) => {
   const { ...rest } = req.body;
   let data = [];
   try {
-    const getData = await studentdetails.findOne({
+    const userRegister = await register.findOne({
       where: {
         email: rest.email,
       },
     });
-    console.log("email", getData);
-    if (getData == null || getData.email !== rest.email) {
-      const create = await studentdetails.create(req.body);
-      data.push(create);
+    if (userRegister != null || userRegister != undefined) {
+      const getData = await studentdetails.findOne({
+        where: {
+          email: rest.email,
+        },
+      });
+      console.log("email", getData);
+      if (getData == null || getData.email !== rest.email) {
+        const create = await studentdetails.create(req.body);
+        data.push(create);
+      } else {
+        res.send({ msg: "this email is already students" });
+      }
     } else {
-      res.send({ msg: "this email is already students" });
+      res.status(400).json({ msg: "user is not register" });
     }
     res.status(200).json({ msg: "create data successfully", data: data });
   } catch (err) {
@@ -27,16 +36,17 @@ const createStudentsDetails = async (req, res) => {
 
 const getDataAllSt = async (req, res) => {
   try {
-    const data = await db.sequelize.query(`select r.email,r.fname,r.lname,r.username,r.phone,
+    const data = await db.sequelize.query(
+      `select r.email,r.fname,r.lname,r.username,r.phone,
     s.institutionname,s.courseenrolled,s.class,s.section,s.classteacher,s.teacherId,s.role 
     from registers r 
     inner join studentdetails s on s.email =r.email 
     where s.role='Student' && r.isDelete =false && s.isDelete=false  `,
-        {
-            //&& ad.date=${date}
-            type: QueryTypes.SELECT,
-          }
-        );
+      {
+        //&& ad.date=${date}
+        type: QueryTypes.SELECT,
+      }
+    );
     console.log("data", data);
     res.status(200).json({ msg: "student data get successfully", data: data });
   } catch (err) {
@@ -47,15 +57,15 @@ const getDataAllSt = async (req, res) => {
 
 const getDataClassWise = async (req, res) => {
   const { clas, classteacher, teacherId } = req.body;
-  console.log('apidata....',req.body);
+  console.log("apidata....", req.body);
   try {
-    const data = await studentdetails.findAll ({
-      where:{
-        role:'Student',
-        class:clas,
+    const data = await studentdetails.findAll({
+      where: {
+        role: "Student",
+        class: clas,
         isDelete: false,
-      }
-    })
+      },
+    });
     console.log("data", data);
     res.status(200).json({ msg: "student data get successfully", data: data });
   } catch (err) {
@@ -66,15 +76,15 @@ const getDataClassWise = async (req, res) => {
 
 const getDataTeacherWise = async (req, res) => {
   const { clas, classteacher, teacherId } = req.body;
-  console.log('api..',req.body);
+  console.log("api..", req.body);
   try {
-    const data = await studentdetails.findAll ({
-      where:{
-        role:'Student',
-        classteacher:classteacher,
+    const data = await studentdetails.findAll({
+      where: {
+        role: "Student",
+        classteacher: classteacher,
         isDelete: false,
-      }
-    })
+      },
+    });
     console.log("data", data);
     res.status(200).json({ msg: "student data get successfully", data: data });
   } catch (err) {
@@ -89,7 +99,7 @@ const getDetailById = async (req, res) => {
     const getDataById = await studentdetails.findOne({
       where: {
         email: email,
-        isDelete:false
+        isDelete: false,
       },
     });
     res
@@ -104,7 +114,7 @@ const updateDetailsSt = async (req, res) => {
   const { ...rest } = req.body;
   try {
     const data = {
-        name:rest.name,
+      name: rest.name,
       phone: rest.phone,
       institutionname: rest.institutionname,
       courseenrolled: rest.courseenrolled,
@@ -125,23 +135,25 @@ const updateDetailsSt = async (req, res) => {
     res.status(500).json({ msg: "students details not update", err });
   }
 };
-const deteteDetailsSt  = async (req,res) => {
-    const {email} =req.body;
-    try{
-        const data={
-            isDelete:true
-        }
-const deleteData = await studentdetails.update(data,{
-    where: {
+const deteteDetailsSt = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const data = {
+      isDelete: true,
+    };
+    const deleteData = await studentdetails.update(data, {
+      where: {
         email,
       },
-})
-res.status(200).json({msg:'students details delete successfully',data:deleteData})
-    }catch(err){
-        console.log(err);
-        res.status(500).json({msg:'students details not delete ..',err})
-    }
-}
+    });
+    res
+      .status(200)
+      .json({ msg: "students details delete successfully", data: deleteData });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "students details not delete ..", err });
+  }
+};
 
 module.exports = {
   createStudentsDetails,
@@ -150,5 +162,5 @@ module.exports = {
   updateDetailsSt,
   deteteDetailsSt,
   getDataClassWise,
-  getDataTeacherWise
+  getDataTeacherWise,
 };
